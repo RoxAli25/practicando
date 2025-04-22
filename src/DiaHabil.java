6f191ef6e436c0079fb172bac10b6f632387975b
Roxana Aliaga <andrea.200210@gmail.com>
2025-04-22 11:53:07 -0400

    desafio terminado

--- src/DiaHabil.java
+++ src/DiaHabil.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class DiaHabil {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        //Se le pide al Usuario ingresar un Día
+        System.out.println("Ingrese un día de la Semana");
+        String dia = scanner.nextLine().toLowerCase(); //El "toLowerCase()" se usa para convertir lo que se escriba en minuscula
+
+        //Se Verifica el Día
+        if (dia.equals("lunes") || dia.equals("martes") || dia.equals("miercoles") || dia.equals("jueves")
+                || dia.equals("viernes") ) {
+            System.out.println(dia + " Es Día Hábil");
+        } else if (dia.equals("sabado") || dia.equals("domingo")){
+            System.out.println(dia + " No es día Hábil");
+        } else {
+            System.out.println("Opción Ingresada NO es válida");
+        }
+
+        scanner.close();
 
     }
 }
