69635f242d597fb442828bad83e9fdd363b97f44
Roxana Aliaga <andrea.200210@gmail.com>
2025-04-22 10:51:06 -0400

    Repasando

---
+++ src/ParOImpar.java
@@ -0,0 +1,12 @@
+public class Practicando {
+
+    public static void main(String[] args) {
+        int valor = 8;
+        if (valor % 2 == 0) {
+            System.out.println("El número " + valor + " es par.");
+        } else {
+            System.out.println("El número " + valor + " es impar.");
+        }
+    }
+}
+
